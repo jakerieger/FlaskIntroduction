8d67ff977f0db71e1f05c0b26959d2edee384ce5
Jake Rieger <contact.jakerieger@gmail.com>
2019-05-28 22:10:11 -0400

    removed 'completed' column

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
