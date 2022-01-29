c118eb26efee085b7ad0976d3762497acbe85588
kcfigaro <kcfigaro@gmail.com>
2022-01-29 22:50:56 +0100

    updated file

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+from sqlalchemy import false
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = false
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
